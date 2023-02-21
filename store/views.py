f5d04425596d3b5ba70f03ecd67e090d79003f82
User anna_kaname <nastyapiar@gmail.com>
2023-02-21 16:00:47 +0500

    html файлы

--- store/views.py
+++ store/views.py
@@ -6,5 +6,7 @@ from core import settings
 
 def all_products(request):
     products = Product.objects.all()
-    context = {'products': products}
+    category = Product.objects.filter(in_stock=True).select_related('category')
+    context = {'products': products, 'category':category}
     return render(request, 'store/home.html', context)
+
